439ad6271bb11e0a3630a67a5a74ff56f695a266
Roberto <ibaomar@hotmail.com>
2022-04-15 09:06:24 +0200

    put finished

--- site/put.py
+++ site/put.py
@@ -16,37 +16,61 @@
 #   Content-Location: /new.html
 
 import os
+import error
 
 def PUT_File(filename, host,protocol, content_type, body):
-    path = os.path.join(host, filename[1:])
+    # In case of '/' we get entry_point_file from the vhosts.conf file
+    if filename == '/':
+        # Open vhosts.conf file
+        vin = open('vhosts.conf')
+        vhosts = vin.readline()
+        # Search for the host in the file vhost.conf
+        while vhosts:
+            vhosts = vhosts.split(",")
+            if vhosts[0] == host:
+                filename = '/' + vhosts[1]                
+            vhosts = vin.readline()
+        vin.close()
+        # If it didn't found it then it means that host is not supported
+        if filename == '/':
+            return(error.error_handling(404, protocol))
+
+    #path = os.path.join(host, filename[1:])
+    #path = ("/" + path)
+    path = os.path.join("/",filename[1:])
     #if os.path.exists():
     name, extension = os.path.splitext(path)
+
     if extension == ".html":
         request_content_type = "text/html"
         data = open(path, "w")
-        data.write(body)
+        for i in range(0, len(body)):
+            data.write(body[i])
         data.close()
     elif extension == ".txt":
         request_content_type = "text/plain"
         data = open(path, "w")
-        data.write(body)
+        for i in range(0, len(body)):
+            data.write(body[i])
         data.close()
     elif extension == ".jpeg":
         request_content_type = "image/jpeg"
         data = open(path, "wb")
-        data.write(body)
+        for i in range(0, len(body)):
+            data.write(body[i])
         data.close()
     elif extension == ".png":
         request_content_type = "image/png" 
         data = open(path, "wb")
-        data.write(body)
+        for i in range(0, len(body)):
+            data.write(body[i])
         data.close()
     else:
         request_content_type = "Type Not supported" 
         
     response_header = []
     response_status = " ".join([protocol, "201", "Created"])
-    response_location = " ".join(["Content-Location:", filename] )
+    response_location = " ".join(["Content-Location:", path] )
     response_header.append(response_location)
       
     request_headers = "\r\n".join(response_header)
